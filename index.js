f5139996a59b63efe868f56abcc9f03c59933b21
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-27 13:20:48 -0500

    commit tree 69e5acd8c83470cfc18e164472236df1a705bd7c

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+  const newArray =Array.from(word) 
+  const reversedArray= newArray.reverse()
+  const reversedWord = reversedArray.join("")
+  if (word === reversedWord) {return true}
+  else {return false}
+  
+  
+  
+  }
+  
 
 module.exports = isPalindrome;
+
+
